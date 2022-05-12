e985241b52e12aadeda45b2d58ffb9b986524ca4
Mansoor Akhter <mansoor21st@Gmail.com>
2022-05-12 15:48:12 +0500

    Yelp API & Google API
    Working

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,36 @@
 import { View, Text, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
 import RestaurantItems, { localRestaurants } from '../components/RestaurantItems'
 
-export default function Home() {
+const YELP_API_KEY =
+  "1pThvy2f77-an8XhG-tjHfFhNkRFdx1PNCTo92bSZnXp4w4jnA0m_U8_bor6A0r95yHmKV5yzk1C70JrCtaNZzsfuvncg-h1MMcuibOaJbX_TP181S3QLXgjNN58YnYx";
 
+
+export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=Newyork";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    }
+
+    return fetch(yelpUrl, apiOptions).then((res) => res.json()).then(json => setRestaurantData(json.businesses))
+
+  };
+
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
+
 
   return (
     <View style={{ backgroundColor: "#eee", flex: 1, width: "100%" }}>
